8f19eced526fe8e0056f3d209d9b1303887acf2a
Olalere Kareem <olalereolatunbosun@gmail.com>
2022-11-22 13:26:36 -0800

    done

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
-const express = require("express")
-const app = express()
+const express = require('express');
+const app = express();
 const nodemailer = require('nodemailer');
-require("dotenv/config")
+require('dotenv/config');
 
-const cors = require("cors");
+const cors = require('cors');
 
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-app.use(cors())
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
+const add = require('./routes/add');
+const substrate = require('./routes/substrate');
 
-const add = require("./routes/add")
-const substrate = require("./routes/substrate")
-
-app.use("/add", add)
-app.use("/substrate", substrate)
-
-
+app.use('/add', add);
+app.use('/substrate', substrate);
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
+  host: 'smtp.gmail.com',
   port: 465,
   secure: true, // use TLS
   auth: {
-    user: "grudge.m3district@gmail.com",
+    user: 'grudge.m3district@gmail.com',
     pass: process.env.EMAIL_PASSWORD,
   },
   tls: {
@@ -32,31 +29,34 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-
 // hahahahahahahahahahahahahahahahah
 transporter.verify(function (error, success) {
   if (error) {
     console.log(error);
   } else {
-    console.log("Server is ready to take our messages");
+    console.log('Server is ready to take our messages');
   }
 });
 
-// transporter.sendMail({
-//   from: '"Node tes👻" <foo@example.com>', // sender address
-//   to: "xyz@gmail.com", // list of receivers
-//   subject: "we are good to go", // Subject line
-//   // text: "Trying from the server with env keys", // plain text body
-//   html: "<b>Hello from Miebaka</b>", // html body
-// }).then(info => {
-//   console.log(info.response);
-//   console.log(info.messageId);
-// })
+app.get('/verifyEmail', (req, res) => {
+  transporter
+    .sendMail({
+      from: '"Node tes👻" <foo@example.com>', // sender address
+      to: 'grudge.m3district@gmail.com', // list of receivers
+      subject: 'we are good to go', // Subject line
+      // text: "Trying from the server with env keys", // plain text body
+      html: '<b>Hello from Miebaka</b>', // html body
+    })
+    .then(() => {
+      res.status(200).json({
+        status: 'Success',
+        message: 'Email sent Successfully',
+      });
+    });
+});
 
-const PORT = process.env.PORT || 3001
-
-
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-  console.log("Server is running and open " + PORT)
-})
+  console.log('Server is running and open ' + PORT);
+});
