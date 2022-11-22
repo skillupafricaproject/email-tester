58e2d24d90398f551560745b9eb6a7b4506dd956
ZoDD-Script <blessedchukwu89@gmail.com>
2022-11-22 21:57:38 +0100

    added divide folder

---
+++ routes/divide.js
@@ -0,0 +1,11 @@
+const router = require("express").Router()
+
+
+router.post("/", (req, res) => {
+    const {div1, div2} = req.body
+    const result = div1 * div2
+    res.json({message: `The solution is ${result}`})
+})
+
+
+module.exports = router
